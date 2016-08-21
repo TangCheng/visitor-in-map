0e80fd475488404fbad0ee6a519ee1229645a1c8
Fengda HUANG <h@phodal.com>
2016-08-21 08:30:27 +0800

    log ip

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function(req, res) {
     console.log(req.body);
+    // var ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
+    // console.log(ip);
     res.send('success');
 });
 
