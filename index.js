0678a69515901f887c683ef8f728cd04616f0797
Fengda HUANG <h@phodal.com>
2016-08-21 08:39:53 +0800

    init frontend

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 app.post('/', function(req, res) {
     console.log(req.body);
